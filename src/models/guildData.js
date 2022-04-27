3a3f7feb0552f91ec3bac167626b90fade427d45
Pauldb09 <pauldeboissieu@yahoo.com>
2022-04-27 18:44:09 +0200

    big update

--- src/models/guildData.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require("mongoose"),
-    model = new mongoose.Schema({
-        serverId: { type: String, required: true },
-        modlogs: { type: Object, required: false },
-        cases: { type: Array, required: false, default: [] },
-    });
-module.exports = mongoose.model("guildData", model);
